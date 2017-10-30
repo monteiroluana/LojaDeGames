320780fe05660c22bdffdd6421ac8c89d5a164aa
Jonas-Ribeiro <jonas.aribeiro@outlook.com>
2017-10-30 00:12:31 -0200

    tese
    vai tteia

---
+++ LojaDeGames/src/br/com/lojaGame/service/cliente/ServicoCliente.java
@@ -0,0 +1,116 @@
+package br.com.lojaGame.service.cliente;
+
+import br.com.lojaGame.exceptions.ClientesException;
+import br.com.lojaGame.mock.MockCliente;
+import br.com.lojaGame.exceptions.DataSourceException;
+import br.com.lojaGame.model.cliente.Cliente;
+import br.com.lojaGame.model.validador.cliente.ValidadorCliente;
+import java.util.List;
+
+public class ServicoCliente {
+
+    public static void cadastrarCliente(Cliente cliente) {
+        throws ClientesException, DataSourceException
+        {
+
+            ValidadorCliente.validar(cliente);
+
+            try {
+
+                MockCliente.inserir(cliente);
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                throw new DataSourceException("Erro na fonte de dados", e);
+
+            }
+        }
+        
+         
+
+    
+
+    public static void atualizarCliente(Cliente cliente) {
+        throwsClientesException, DataSourceException
+        {
+
+            ValidadorCliente.validar(cliente);
+
+            try {
+
+                MockCliente.atualizar(cliente);
+                return;
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                throw new DataSourceException("Erro na fonte de dados", e);
+
+            }
+        }
+    }
+
+    //
+    public static List<Cliente> procurarCliente(String nome) {
+        throwsClientesException, DataSourceException
+        {
+
+            try {
+                if (nome == null || "".equals(nome)) {
+
+                    return MockCliente.listar();
+                } else {
+
+                    return MockCliente.procurar(nome);
+                }
+                catch(Exception e){
+                    
+                    e.printStacTrace();
+                    throw new DataSourceException("Erro na fonte de dados", e);
+                    }
+            }
+        
+        
+
+    public static Cliente obterCliente(int id) {
+        throwsClientesException, DataSourceException
+        {
+
+            try {
+
+                return MockCliente.obter(id);
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                throw new DataSourceException("Erro na fonte de dados", e);
+            }
+        }
+//teste
+    }
+
+    public static void excluirCliente(int id) {
+        throwsClientesException, DataSourceExcepetion
+        {
+
+            try {
+                MockCliente.excluir(id);
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                throw new DataSourceException("Erro nda fonte de dados", e);
+            }
+
+        }
+
+    }
+
+        
+        
+                
+    
+    }
+    
+ 
+    
+
