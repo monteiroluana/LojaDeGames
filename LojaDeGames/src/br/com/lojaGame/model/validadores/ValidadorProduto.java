a516f2513fde54d7506537401f405c5502d3dd36
Bruna Sayuri <bruna_susuke@hotmail.com>
2017-12-12 17:00:25 -0200

    validador plataforma att

--- LojaDeGames/src/br/com/lojaGame/model/validadores/ValidadorProduto.java
+++ LojaDeGames/src/br/com/lojaGame/model/validadores/ValidadorProduto.java
@@ -27,18 +27,19 @@ public class ValidadorProduto {
         }
         if (produto.getFaixaEtaria() == null || "Selecione".equals(produto.getFaixaEtaria())) {
             throw new ProdutosException("É necessário informar a faixa etária do jogo");
-        }        
+        }
         if (produto.getPreco() == null || produto.getPreco() <= 0) {
             throw new ProdutosException("É necessário informar o preço do jogo");
-        }        
+        }
         if (produto.getQtdEstoque() == null || produto.getQtdEstoque() < 0) {
             throw new ProdutosException("Campo estoque está vazio");
-        }        
-        if ("Selecione".equals(produto.getQtdJogadores())){
+        }
+        if ("Selecione".equals(produto.getQtdJogadores())) {
             produto.setQtdJogadores(null);
         }
     }
-     public static void validarAtualizacao(Produto produto) throws ProdutosException, Exception {
+
+    public static void validarAtualizacao(Produto produto) throws ProdutosException, Exception {
         if (produto == null) {
             throw new ProdutosException("Não foi informado um produto");
         }
@@ -54,19 +55,19 @@ public class ValidadorProduto {
         if (produto.getCategoria() == null || "Selecione...".equals(produto.getCategoria())) {
             throw new ProdutosException("É necessário informar a categoria do jogo");
         }
-        if (produto.getPlataforma() == null || "Selecione".equals(produto.getPlataforma())) {
+        if (produto.getPlataforma() == null || produto.getPlataforma() == 0) {
             throw new ProdutosException("É necessário informar a plataforma do jogo");
         }
         if (produto.getFaixaEtaria() == null || "Selecione".equals(produto.getFaixaEtaria())) {
             throw new ProdutosException("É necessário informar a faixa etária do jogo");
-        }        
+        }
         if (produto.getPreco() == null || produto.getPreco() <= 0) {
             throw new ProdutosException("É necessário informar o preço do jogo");
-        }        
+        }
         if (produto.getQtdEstoque() == null || produto.getQtdEstoque() < 0) {
             throw new ProdutosException("Campo estoque está vazio");
-        }        
-        if ("Selecione".equals(produto.getQtdJogadores())){
+        }
+        if ("Selecione".equals(produto.getQtdJogadores())) {
             produto.setQtdJogadores(null);
         }
     }
